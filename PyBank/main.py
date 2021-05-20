7df8e206552aaf174ace11a9f136f2333b1c6314
Sam Pierce <sdinespierce@gmail.com>
2021-05-20 08:41:49 -0400

    updated main.py in PyPoll and PyBank with algorithms in pseudocode

---
+++ PyBank/main.py
@@ -0,0 +1,31 @@
+#import dependencies
+
+#read in csv
+
+#initialize variables
+
+
+#loop through rows
+    #count all rows
+    #total_pro_lo += current_pro_lo
+    #if current_pro_lo > 0:
+        #if current_pro_lo > greatest_increase_amt:
+            #greatest_increase_amt = current_pro_lo
+            #greatest_increase_month = current_month
+    #elif current_pro_lo < 0:
+        #if current_pro_lo < greatest_decrease:
+            #greatest_decrease_amt = current_pro_lo
+            #greatest_decrease_month = Current_month
+
+#avg_monthly_pro_lo = total_pro_lo / month_count
+
+#output
+    #total num of months
+    #net profits/losses
+    #change in profits/losses over whole period
+        #average those changes
+    #greatest increase in profits
+    #greatest decrease in profits
+
+    #print
+    #write to new txt file
---
+++ PyPoll/main.py
@@ -0,0 +1,29 @@
+#import dependencies
+
+#initialize variables
+#name_list
+#num of votes list
+#row_count = 0
+
+#loop rows
+    #row_count += 1
+    #if candidate_name in name_list:
+        #find index in name list
+        #increment num of votes at index
+    #else
+        #add candidate name to name list
+        #get that index
+        #increment num of votes at new index
+
+#calculations
+#loop name list
+    #get name
+    #get vote nums
+    #get % of total
+    #compare vote nums to find winner
+    #add to output string
+
+
+#output
+#print output
+#write to txt file
